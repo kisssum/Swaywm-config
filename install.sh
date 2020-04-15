7447dd3b0bcbae24fcf7ab1ee18602cfff63a491
kisssum <kisssum@ENIAC.localdomain>
2020-04-15 20:26:32 +0800

    update

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@ if [ ! -d ~/.config ]
 then
   mkdir ~/.config
   cp ./terminal/alacritty ~/.config/
-  cp ./terminal/xfce4/* ~/.config/xfce4/terminal/
   cp ./rofi ~/.config/
   cp ./sway ~/.config/
   cp ./ ~/.config/
@@ -13,4 +12,6 @@ fi
 if [ ! -d ~/.config/xfce4 ]
 then
   mkdir -p ~/.config/xfce4/terminal
+else
+  cp ./terminal/xfce4/* ~/.config/xfce4/terminal/
 fi
